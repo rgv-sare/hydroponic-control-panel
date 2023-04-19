b0e45e18f97398e9b429a2c8084b81ac2179cca3
elmfrain <elmfer10@gmail.com>
2023-04-19 01:57:56 -0500

    Setup For Creating Windows

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    glfwInit();
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Hydroponics Control Panel", NULL, NULL);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
 
     return 0;
 }

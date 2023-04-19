14e658fa1b6954ae94b02b6dab92ef64de6c96be
elmfrain <elmfer10@gmail.com>
2023-04-19 02:04:14 -0500

    OpenGL Setup For Drawing Graphics

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include <glad/glad.h>
+
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
 int main()
@@ -8,8 +11,14 @@ int main()
 
     GLFWwindow* window = glfwCreateWindow(1280, 720, "Hydroponics Control Panel", NULL, NULL);
 
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+
     while(!glfwWindowShouldClose(window))
     {
+        glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwPollEvents();
         glfwSwapBuffers(window);
     }

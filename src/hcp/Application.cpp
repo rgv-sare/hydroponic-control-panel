36b2f5cb299c135b2217f890c0d806814e99fdf3
elmfrain <elmfer10@gmail.com>
2023-05-02 03:21:58 -0500

    Application: clear stencil on every frame

--- src/hcp/Application.cpp
+++ src/hcp/Application.cpp
@@ -45,7 +45,7 @@ void HCPApplication::loop()
 {
     glViewport(0, 0, hcpui::getWindowWidth(), hcpui::getWindowHeight());
     glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
     if(m_currentScreen)
         m_currentScreen->draw();

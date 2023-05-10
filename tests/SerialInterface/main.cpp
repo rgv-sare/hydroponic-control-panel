7d0c9a099c62c5e441511bd5c58777e80a33621b
elmfrain <elmfer10@gmail.com>
2023-05-09 21:17:42 -0500

    Variables get interface test

--- tests/SerialInterface/main.cpp
+++ tests/SerialInterface/main.cpp
@@ -1,11 +1,15 @@
 #include "hcp/SystemInterface.hpp"
 #include "hcp/Serial.hpp"
 
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 #include <thread>
 
 int main(int argc, char** argv)
 {
+    glfwInit();
+
     std::string port;
     if(argc != 2)
     {
@@ -20,7 +24,6 @@ int main(int argc, char** argv)
     }
     else
         port = argv[1];
-    port = "COM3";
 
     std::cout << "Starting on port " << port << '\n';
     hcpsi::startOnPort(port.c_str());
@@ -28,6 +31,38 @@ int main(int argc, char** argv)
     std::cout << "Waiting for connection...\n";
     while (!hcpsi::isAlive() && !hcpsi::failed())
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    std::cout << "Getting variables...\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000)); 
+    std::vector<std::string> vars = hcpsi::getVariables();
+
+    std::cout << "Variables:\n";
+
+    for (auto& var : vars)
+    {
+        hcpsi::Var& v = hcpsi::getVariable(var.c_str());
+        std::cout << var << ": \n";
+        std::cout << "\tType: " << v.type << '\n';
+
+        switch(v.type)
+        {
+        case hcpsi::Type::BYTE:
+            std::cout << "\tValue: " << (int)v.byte << '\n';
+            break;
+        case hcpsi::Type::SHORT:
+            std::cout << "\tValue: " << v.word << '\n';
+            break;
+        case hcpsi::Type::INT:
+            std::cout << "\tValue: " << v.dword << '\n';
+            break;
+        case hcpsi::Type::FLOAT:
+            std::cout << "\tValue: " << v.fword << '\n';
+            break;
+        case hcpsi::Type::STRING:
+            std::cout << "\tValue: " << v.data << '\n';
+            break;
+        }
+    }
 
     std::cout << "Stopping...\n";
     hcpsi::stop();
